f4fe2ac12c6e6620141792230b52bd7a7cce0cd8
walethesolution <olawaleayejuyone@gmail.com>
2024-09-19 11:45:48 -0400

    Add About changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,16 +11,14 @@ const ServiceCard = ({ index, title, icon }) => {
   return (
     <motion.div
       variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
-      className="xs:w-[250px] w-full  p-[1px] rounded-[20px]"
-    >
+      className="xs:w-[250px] w-full  p-[1px] rounded-[20px]">
       <div
         options={{
           max: 45,
           scale: 1,
           speed: 450,
         }}
-        className="bg-[#4d718b] rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
-      >
+        className="bg-[#4d718b] rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col">
         <img src={icon} alt={title} className="w-16 h-16 object-contain" />
         <h3 className="text-[#e6ebee] text-[18px] font-bold text-center">
           {title}
@@ -44,34 +42,43 @@ const About = () => {
 
       <motion.p
         variants={fadeIn("", "", 0.1, 1)}
-        className="mt-4 text-taupe text-[18px] max-w-3xl leading-[30px]"
-      >
-        Experienced Web Developer with a proven track record of five years in the industry. Proficient in both front-end and back-end technologies, specializing in HTML, CSS, JavaScript, and popular frameworks like React. Adept at creating responsive and performant web applications, with a strong foundation in database management and API integration. Skilled in version control, testing, and deployment, emphasizing security best practices. Well-versed in collaborating with cross-functional teams and contributing to all phases of the development lifecycle. Committed to continuous learning and staying abreast of emerging trends. Bring a combination of technical expertise, problem-solving skills, and effective communication to drive successful web development projects
+        className="mt-4 text-taupe text-[18px] max-w-3xl leading-[30px]">
+        Experienced Web Developer with a proven track record of five years in
+        the industry. Proficient in both front-end and back-end technologies,
+        specializing in HTML, CSS, JavaScript, and popular frameworks like
+        React. Adept at creating responsive and performant web applications,
+        with a strong foundation in database management and API integration.
+        Skilled in version control, testing, and deployment, emphasizing
+        security best practices. Well-versed in collaborating with
+        cross-functional teams and contributing to all phases of the development
+        lifecycle. Committed to continuous learning and staying abreast of
+        emerging trends. Bring a combination of technical expertise,
+        problem-solving skills, and effective communication to drive successful
+        web development projects
       </motion.p>
       <motion.p
         variants={fadeIn("", "", 0.1, 1)}
-        className="mt-4 text-taupe text-[18px] max-w-3xl leading-[30px]"
-      >
-        My software development journey began with a fascination for how
-        lines of code turn into powerful tools that can change how we live and work.
+        className="mt-4 text-taupe text-[18px] max-w-3xl leading-[30px]">
+        My software development journey began with a fascination for how lines
+        of code turn into powerful tools that can change how we live and work.
         Over the years I have specialized in full stack development and
         contributed to numerous projects that have made meaningful impact.
         <br />
       </motion.p>
       <motion.p
         variants={fadeIn("", "", 0.1, 1)}
-        className="mt-4 text-taupe text-[18px] max-w-3xl leading-[30px]"
-      >
+        className="mt-4 text-taupe text-[18px] max-w-3xl leading-[30px]">
         What keeps me motivated is the ever-evolving nature of technology and
-        the boundless possibilities it offers. I&apos;m driven by the bringing to life of a vision through lines of code and creating user-friendly
+        the boundless possibilities it offers. I&apos;m driven by the bringing
+        to life of a vision through lines of code and creating user-friendly
         applications that make a real difference in people&apos;s lives.
       </motion.p>
 
       <motion.p
         variants={fadeIn("", "", 0.1, 1)}
-        className="mt-4 text-taupe text-[18px] max-w-3xl leading-[30px]"
-      >
-        When I&apos;m not coding, i&apos;m either playing soccer or basketball
+        className="mt-4 text-taupe text-[18px] max-w-3xl leading-[30px]">
+        When I&apos;m not coding, i&apos;m building a community of fit and happy
+        people.
       </motion.p>
 
       <div className="mt-20 flex flex-wrap gap-10">
